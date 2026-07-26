52d5a3862ba5c240a9e2af0928e89fa90df6079a
hyunsuhne <sungkibae@gmail.com>
2026-07-27 08:56:29 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { pickQuestions, buildQuestionFromWord } from './wordbank.js?v=13.13';
+import { pickQuestions, buildQuestionFromWord } from './wordbank.js?v=1.13';
 import { getResultGrade, getRecommendation } from './vocabulary.js';
 import { getResultGrade2, getRecommendation2 } from './vocabulary2.js';
 import { getResultGrade3, getRecommendation3 } from './vocabulary3.js';
