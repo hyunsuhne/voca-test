5ffd5e49104dbc424ea4661bbb2a14b89f8d1f09
hyunsuhne <sungkibae@gmail.com>
2026-08-19 14:43:45 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-import { pickQuestions, buildQuestionFromWord, AUDIO_WORDS } from './wordbank.js?v=1.23';
-import { track, submitResult, fetchScoreStats } from './analytics.js?v=1.23';
+import { pickQuestions, buildQuestionFromWord, AUDIO_WORDS } from './wordbank.js?v=1.24';
+import { track, submitResult, fetchScoreStats } from './analytics.js?v=1.24';
 import { getResultGrade, getRecommendation } from './vocabulary.js';
 import { getResultGrade2, getRecommendation2 } from './vocabulary2.js';
 import { getResultGrade3, getRecommendation3 } from './vocabulary3.js';
